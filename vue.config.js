8584382d5a9eb6a73a633cf19d0f59a5e61edea4
dimasikshemetoff <dshemetoff@gmail.com>
2025-04-02 22:54:08 +0300

    поработал над веб приложением

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pwa: {
+    name: 'Манго',
+    themeColor: '#42B983',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black-translucent',
+    manifestOptions: {
+      background_color: '#FFFFFF'
+    }
+  }
 })
